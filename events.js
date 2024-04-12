80401db78aa58d24d540bd5a5a1e39c26f6b67cf
ka-suratcha <ka.suratcha@gmail.com>
2024-04-12 13:52:53 +0700

    EventEmitter with http
    - try EventEmitter with http (built-in module) to see how it work

--- events.js
+++ events.js
@@ -3,9 +3,11 @@
 // multiple listener for the same event -> will run sync
 
 // EventEmiiter kind of standard name
-const EventEmitter = require("events");
+const EventEmitter = require('events');
+const http = require('http')
 
 // best practice is create a new class then inherit from the node EventEmitter
+// extending the EventEmitter class is how the differnt node modules implement event internally
 // parent is sales, superclass is EventEmitter
 class Sales extends EventEmitter {
     constructor() {
@@ -17,17 +19,38 @@ class Sales extends EventEmitter {
 const myEmitter = new Sales();
 
 // create listener (ovserve the emitter) wait for emit the newSale event
-myEmitter.on("newSale", () => {
-    console.log("There was a new sale!");
+myEmitter.on('newSale', () => {
+    console.log('There was a new sale!');
 });
 
-myEmitter.on("newSale", () => {
-    console.log("Costumer name: Jonas");
+myEmitter.on('newSale', () => {
+    console.log('Costumer name: Jonas');
 });
 
-myEmitter.on("newSale", (stock) => {
+myEmitter.on('newSale', (stock) => {
     console.log(`There r now ${stock} items left in stock`);
 });
 
 // emit the events
-myEmitter.emit("newSale", 9);
+myEmitter.emit('newSale', 9);
+
+// =============================================================
+
+const server = http.createServer();
+
+server.on('request', (req, res) => {
+    res.end('Request received');
+    console.log(req.url);
+})
+
+server.on('request', (req, res) => {
+    console.log('Another request');
+})
+
+server.on('close', () => {
+    console.log('Server closed');
+})
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Waiting for requests...');
+})
