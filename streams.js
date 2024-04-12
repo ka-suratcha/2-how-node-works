113c101a07693226049ca3d66417a1f9cd783f8c
ka-suratcha <ka.suratcha@gmail.com>
2024-04-12 14:24:42 +0700

    3th soulution use pipe operator
    - pipe the output of readable stream into the impur of a writable stream
    automatically handle speed of data going out

--- streams.js
+++ streams.js
@@ -12,18 +12,26 @@ server.on('request', (req, res) => {
 
     // SOLUTION 2 streams
     // read one chuck of file then send to client using write method unit entire file is readed
-    const readable = fs.createReadStream('testt-file.txt')
-    readable.on('data', chunk => {
-        res.write(chunk);
-    })
-    readable.on('end', () => {
-        res.end();
-    })
-    readable.on('error', err => {
-        console.log(err);
-        res.statusCode = 500
-        res.end('File not found!')
-    })
+    // const readable = fs.createReadStream('testt-file.txt')
+    // readable.on('data', chunk => {
+    //     res.write(chunk);
+    // })
+    // readable.on('end', () => {
+    //     res.end();
+    // })
+    // readable.on('error', err => {
+    //     console.log(err);
+    //     res.statusCode = 500
+    //     res.end('File not found!')
+    // })
+
+    // SOLUTION 3 pipe operator
+    // fix backpressure
+    // pipe the output of readable stream into the impur of a writable stream
+    // automatically handle speed of data going out
+    const readable = fs.createReadStream('test-file.txt')
+    readable.pipe(res);
+    // readableSouce.pipe(writeavledest)
 })
 
 server.listen(8000, '127.0.0.1', () => {
